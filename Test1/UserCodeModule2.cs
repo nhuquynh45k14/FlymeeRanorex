7a97469c2a342cf3e72c81a6969411f6065da0e9
nhuquynh45k14 <quynhbabe123456789@gmail.com>
2022-11-09 16:18:40 +0700

    Ver1

---
+++ Test1/UserCodeModule2.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by Ranorex
+ * User: PC 2103-0602
+ * Date: 10/19/2022
+ * Time: 9:25 AM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace Test1
+{
+    /// <summary>
+    /// Description of UserCodeModule2.
+    /// </summary>
+    [TestModule("AE919293-A0F9-4A3F-B0F7-3ECF9E5B8E5F", ModuleType.UserCode, 1)]
+    public class UserCodeModule2 : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public UserCodeModule2()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+        }
+    }
+}
